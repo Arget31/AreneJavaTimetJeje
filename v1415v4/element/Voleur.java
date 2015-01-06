f2c89bbc69065c62b98f67633f35a792e12943b6
Arget31 <njeremy12@gmail.com>
2015-01-06 16:08:52 +0100

    src

--- v1415v4/element/Voleur.java
+++ v1415v4/element/Voleur.java
@@ -4,6 +4,7 @@ import interaction.Actions;
 import interaction.Deplacements;
 import interfaceGraphique.VueElement;
 
+import java.awt.Point;
 import java.rmi.RemoteException;
 import java.util.Hashtable;
 
@@ -17,11 +18,17 @@ public class Voleur extends Personnage {
 
 	public void pouvoir(VueElement ve, Integer refRMI, VueElement cible, int refPlusProche,
 			Actions actions, Deplacements deplacements, Hashtable<Integer,VueElement> voisins) throws RemoteException {
-
+			Point tamp = new Point();
 		if(!actions.simulation(refRMI,refPlusProche,ve.getControleur().getArene())){
         		parler("Je me camoufle et me dirige vers mon voisin" + refPlusProche, ve);
 				setInvisibilite(10);
-				deplacements.seDirigerVers(refPlusProche);
+				tamp.x=cible.getPoint().x;
+				tamp.y=cible.getPoint().y;
+				tamp.x=99-tamp.x;
+				tamp.y=99-tamp.y;
+				for(int i=0;i<5;i++){
+					deplacements.seDirigerVers(tamp);
+				}
 				setRechargement(60);
 		}
 		else{
